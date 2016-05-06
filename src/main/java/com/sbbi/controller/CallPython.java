e9feab9f07ecf1582feb9c609b5cfa11ce8023cd
Bruno Silva <bsilva@cse.unl.edu>
2016-05-06 14:23:51 -0500

    talking to python and receiving # of features

--- src/main/java/com/sbbi/controller/CallPython.java
+++ src/main/java/com/sbbi/controller/CallPython.java
@@ -1,10 +1,34 @@
 package com.sbbi.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CallPython {
 
 	private final String PATH = "/home/bsilva/Desktop/sbbi/foodEngine/engine.py";
 	
 	public void call(){
+		
+		String photo = "/home/bsilva/Desktop/sbbi/foodEngine/building.jpg";
+		
+		
+		//long currentTimeMillis = System.currentTimeMillis();
+		
+		 ProcessBuilder pb = new ProcessBuilder("python", PATH, photo);
+		 
+		 try {
+			Process p = pb.start();
+			
+			 BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			 System.out.println(in.readLine());
+			 //int ret = new Integer(in.readLine());
+			 //System.out.println("value is : "+ret);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
