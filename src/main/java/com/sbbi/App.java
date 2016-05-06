58dbff0904a0a9ab57e1f94bc90ef7d2cd0c0c63
Bruno Silva <bsilva@cse.unl.edu>
2016-05-06 13:55:57 -0500

    init

---
+++ src/main/java/com/sbbi/App.java
@@ -0,0 +1,13 @@
+package com.sbbi;
+
+import com.sbbi.controller.CallPython;
+
+public class App {
+	
+    public static void main( String[] args ){
+        
+    	CallPython c = new CallPython();
+    	c.call();
+    	
+    }
+}
---
+++ src/main/java/com/sbbi/controller/CallPython.java
@@ -0,0 +1,11 @@
+package com.sbbi.controller;
+
+public class CallPython {
+
+	private final String PATH = "/home/bsilva/Desktop/sbbi/foodEngine/engine.py";
+	
+	public void call(){
+		
+	}
+	
+}
