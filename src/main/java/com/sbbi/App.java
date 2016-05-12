7f45daee9a786023a7a3177e9f8e5d796b3c91d1
Bruno Silva <bsilva@cse.unl.edu>
2016-05-12 17:30:09 -0500

    similarity

--- src/main/java/com/sbbi/App.java
+++ src/main/java/com/sbbi/App.java
@@ -1,13 +1,52 @@
 package com.sbbi;
 
+import java.math.BigDecimal;
+
 import com.sbbi.controller.CallPython;
+import com.sbbi.dao.connection.MongoConnection;
+import com.sbbi.model.MenuItem;
+import com.sbbi.model.Restaurant;
+import com.sbbi.similarity.Similarity;
 
 public class App {
 	
     public static void main( String[] args ){
         
     	CallPython c = new CallPython();
-    	c.call();
+    	//String[] features = c.call();
+    	
+    	MongoConnection mongo = new MongoConnection();
+    	
+    	/*MenuItem menuItem = new MenuItem();
+    	menuItem.setName("strogonoff 2");
+    	menuItem.setFeatures(features);
+    	mongo.insertMenuItem(menuItem, "Rodizio Grill");*/
+    	
+    	String listRestaurants[] = {"Noodles Company"};
+    	String features[] = new String[1];
+    	calcSimilarity(features, listRestaurants);
+    	
+    	//Restaurant r = mongo.getRestaurant("Subway");
+    	
+    	/*String restaurantName = "Noodles Company";
+		String menuItemName = "pasta";
+		mongo.deleteItemMenu(restaurantName, menuItemName);*/
+    	
+    	/*Restaurant restaurant = new Restaurant();
+    	restaurant.setName("Rodizio Grill");
+    	mongo.insertRestaurant(restaurant);*/
     	
     }
+
+	private static void calcSimilarity(String[] features, String[] listRestaurants) {
+		
+		MongoConnection mongo = new MongoConnection();
+		Similarity similarity = new Similarity();
+		
+		for(String restaurantStr : listRestaurants){
+			Restaurant restaurant = mongo.getRestaurant(restaurantStr);
+			System.out.println("oi");
+		}
+		
+	}
 }
--- src/main/java/com/sbbi/controller/CallPython.java
+++ src/main/java/com/sbbi/controller/CallPython.java
@@ -3,14 +3,21 @@ package com.sbbi.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sbbi.dao.connection.MongoConnection;
 
 public class CallPython {
 
+	private final int FEATURES_SIZE = 180856; 
 	private final String PATH = "/home/bsilva/Desktop/sbbi/foodEngine/";
+	private final String ENGINE_PATH = PATH + "engine.py";
+	
+	public String[] call() {
 
-	public void call() {
-
-		String photoDir = "/home/bsilva/Desktop/sbbi/foodEngine/building.jpg";
+		String photoDir = "/home/bsilva/Desktop/strog1.jpg";
 
 		long currentTimeMillis = System.currentTimeMillis();
 		System.out.println("current time: " + currentTimeMillis);
@@ -18,28 +25,39 @@ public class CallPython {
 
 		//ProcessBuilder pb = new ProcessBuilder("python", PATH + "engine.py", photoDir, currentTimeMillis + "");
 		
+		BigDecimal features[] = new BigDecimal[FEATURES_SIZE];
+		String featuresStr[] = new String[FEATURES_SIZE];
+		
 		try {
 			//Process p = pb.start();
 			
-			String command = "python " + PATH + "engine.py " + photoDir + " " + currentTimeMillis;
+			String command = "python " + ENGINE_PATH + " " + photoDir + " " + currentTimeMillis;
+			System.out.println(command);
 			
 			Process exec = Runtime.getRuntime().exec(command);
 			exec.waitFor();
 			
 			String sCurrentLine;
 
-			br = new BufferedReader(new FileReader(PATH + currentTimeMillis + ".txt"));
-
+			br = new BufferedReader(new FileReader(currentTimeMillis + ".txt"));
+			
+			int i = 0;
+			
 			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+				//features[i] = new BigDecimal(Double.parseDouble(sCurrentLine));
+				featuresStr[i] = sCurrentLine;
+				i++;
 			}
-
+			br.close();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		//return features;
+		return featuresStr;
 
 	}
 }
