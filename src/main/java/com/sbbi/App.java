03522ee02191c7de1145251d68f247e06e77cd1c
Bruno Silva <bsilva@cse.unl.edu>
2016-05-13 11:21:46 -0500

    calculating similarity 1.0

--- src/main/java/com/sbbi/App.java
+++ src/main/java/com/sbbi/App.java
@@ -1,8 +1,6 @@
 package com.sbbi;
 
-import java.math.BigDecimal;
-
-import com.sbbi.controller.CallPython;
+import com.sbbi.controller.ExtractFeature;
 import com.sbbi.dao.connection.MongoConnection;
 import com.sbbi.model.MenuItem;
 import com.sbbi.model.Restaurant;
@@ -12,18 +10,21 @@ public class App {
 	
     public static void main( String[] args ){
         
-    	CallPython c = new CallPython();
-    	//String[] features = c.call();
+    	String photo = "cheessi.jpg";
+    	
+    	ExtractFeature c = new ExtractFeature();
+    	double[] features = c.call(photo);
     	
     	MongoConnection mongo = new MongoConnection();
     	
+    	//save new dish
     	/*MenuItem menuItem = new MenuItem();
-    	menuItem.setName("strogonoff 2");
+    	menuItem.setName("ITpasta");
     	menuItem.setFeatures(features);
-    	mongo.insertMenuItem(menuItem, "Rodizio Grill");*/
+    	mongo.insertMenuItem(menuItem, "Noodles Company");*/
     	
-    	String listRestaurants[] = {"Noodles Company"};
-    	String features[] = new String[1];
+    	//calculate similarity
+    	String listRestaurants[] = {"Noodles Company","Subway","Rodizio Grill"};
     	calcSimilarity(features, listRestaurants);
     	
     	//Restaurant r = mongo.getRestaurant("Subway");
@@ -38,14 +39,19 @@ public class App {
     	
     }
 
-	private static void calcSimilarity(String[] features, String[] listRestaurants) {
+	private static void calcSimilarity(double[] features, String[] listRestaurants) {
 		
 		MongoConnection mongo = new MongoConnection();
 		Similarity similarity = new Similarity();
 		
 		for(String restaurantStr : listRestaurants){
 			Restaurant restaurant = mongo.getRestaurant(restaurantStr);
-			System.out.println("oi");
+			
+			for(MenuItem menuItem : restaurant.getMenuItems()){
+				double value = similarity.calculate(features, menuItem.getFeatures());
+				System.out.println("Similarity with " + menuItem.getName() + ": " + value);
+			}
+			
 		}
 		
 	}
--- src/main/java/com/sbbi/controller/ExtractFeature.java
+++ src/main/java/com/sbbi/controller/ExtractFeature.java
@@ -1,23 +1,19 @@
 package com.sbbi.controller;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.sbbi.dao.connection.MongoConnection;
-
-public class CallPython {
+public class ExtractFeature {
 
 	private final int FEATURES_SIZE = 180856; 
 	private final String PATH = "/home/bsilva/Desktop/sbbi/foodEngine/";
 	private final String ENGINE_PATH = PATH + "engine.py";
 	
-	public String[] call() {
+	public double[] call(String photo) {
 
-		String photoDir = "/home/bsilva/Desktop/strog1.jpg";
+		String photoDir = "/home/bsilva/Desktop/" + photo;
 
 		long currentTimeMillis = System.currentTimeMillis();
 		System.out.println("current time: " + currentTimeMillis);
@@ -25,27 +21,29 @@ public class CallPython {
 
 		//ProcessBuilder pb = new ProcessBuilder("python", PATH + "engine.py", photoDir, currentTimeMillis + "");
 		
-		BigDecimal features[] = new BigDecimal[FEATURES_SIZE];
-		String featuresStr[] = new String[FEATURES_SIZE];
+		//BigDecimal features[] = new BigDecimal[FEATURES_SIZE];
+		double features[] = new double[FEATURES_SIZE];
 		
 		try {
-			//Process p = pb.start();
 			
 			String command = "python " + ENGINE_PATH + " " + photoDir + " " + currentTimeMillis;
 			System.out.println(command);
 			
+			//execute process
 			Process exec = Runtime.getRuntime().exec(command);
 			exec.waitFor();
 			
 			String sCurrentLine;
 
+			//read features from file
 			br = new BufferedReader(new FileReader(currentTimeMillis + ".txt"));
 			
 			int i = 0;
 			
 			while ((sCurrentLine = br.readLine()) != null) {
 				//features[i] = new BigDecimal(Double.parseDouble(sCurrentLine));
-				featuresStr[i] = sCurrentLine;
+				//featuresStr[i] = sCurrentLine;
+				features[i] = Double.parseDouble(sCurrentLine);
 				i++;
 			}
 			br.close();
@@ -57,7 +55,7 @@ public class CallPython {
 		}
 		
 		//return features;
-		return featuresStr;
+		return features;
 
 	}
 }
